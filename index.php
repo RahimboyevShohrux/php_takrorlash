87be6e293c8ef785e4e22bb5279cb692aaa2570e
RahimboyevShohrux <98975918+RahimboyevShohrux@users.noreply.github.com>
2023-07-21 19:23:42 +0500

    Create index.php
    boshlangich tushunchalar

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+$user1="Sardor";
+$user2="Eshmat";
+$user3="Toshmat";
+$narx1="$1500";
+$narx2="$1200";
+$narx3="$2000";
+echo "<table style='width:50%; border:2px solid red; text-align:left; border-collapse:collapse;'>
+    <tr>
+        <th style='border:2px solid red;'>$user1</th>
+        <td style='border:2px solid red;'>$narx1
+        </td>
+    </tr>
+    <tr>
+        <th style='border:2px solid red;'>$user2</th>
+        <td style='border:2px solid red;'>$narx2</td>
+    </tr>
+    <tr>
+        <th style='border:2px solid red;'>$user3</th>
+        <td style='border:2px solid red;'>$narx3</td>
+    </tr>
+</table>"
+?>
